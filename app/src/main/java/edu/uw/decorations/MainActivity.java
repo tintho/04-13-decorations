6bc6733659bf93bb0284b380494835cc21ebb562
Joel Ross <joelross@uw.edu>
2016-04-12 17:20:51 -0700

    Add lecture starter code

---
+++ app/src/main/java/edu/uw/decorations/MainActivity.java
@@ -0,0 +1,28 @@
+package edu.uw.decorations;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Main";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //show fragment
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container, new WordListFragment());
+        ft.commit();
+    }
+
+    //toggles the action bar
+    public void handleButton(View v) {
+
+    }
+
+}
---
+++ app/src/main/java/edu/uw/decorations/WordListFragment.java
@@ -0,0 +1,73 @@
+package edu.uw.decorations;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * A simple Fragment to display a list of words.
+ */
+public class WordListFragment extends Fragment {
+
+    private static final String TAG = "WordList";
+
+    private ArrayAdapter<String> adapter;
+
+    public WordListFragment() {
+        // Required empty public constructor
+    }
+
+    //A factory method to create a new fragment with some arguments
+    public static WordListFragment newInstance() {
+        WordListFragment fragment = new WordListFragment();
+        Bundle bundle = new Bundle();
+        //specify any arguments we want to give to the fragment. These can also come from the method params
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) { /*get values from arguments bundle*/ }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_word_list, container, false);
+
+        //model
+        String[] data = {"Dog","Cat","Android","Inconceivable"};
+
+        //controller
+        AdapterView listView = (AdapterView)rootView.findViewById(R.id.wordListView);
+
+        adapter = new ArrayAdapter<String>(
+                getActivity(), R.layout.list_item_layout, R.id.txtListItem, data);
+        listView.setAdapter(adapter);
+
+
+
+
+        //handle button input
+        final TextView inputText = (TextView)rootView.findViewById(R.id.txtAddWord);
+        Button addButton = (Button)rootView.findViewById(R.id.btnAddWord);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //button click here
+            }
+        });
+
+        return rootView;
+    }
+
+}
